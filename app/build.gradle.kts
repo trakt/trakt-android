7ba9842569bf1b87612dc84deec7bf64c8e427c6
Michal <michal.drabik0@gmail.com>
2025-07-16 16:46:37 +0200

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "tv.trakt.app"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "tv.trakt.app"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlin {
+        jvmToolchain(11)
+    }
+
+    sourceSets {
+        getByName("main") {
+            java.srcDir("${rootDir}/build/generate-resources/main/src/main")
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ build.gradle.kts
@@ -0,0 +1,73 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.openapi.generator)
+}
+
+openApiGenerate {
+    inputSpec = "$rootDir/openapi/openapi.json"
+    generatorName = "kotlin"
+    validateSpec = true
+    cleanupOutput = true
+    generateApiTests = false
+    generateModelTests = false
+    typeMappings = mapOf(
+        "number+int64" to "kotlin.Long",
+    )
+    configOptions = mapOf(
+        "library" to "jvm-ktor",
+        "dateLibrary" to "kotlinx-datetime",
+        "serializationLibrary" to "kotlinx_serialization",
+        "sortModelPropertiesByRequiredFlag" to "true",
+        "sortParamsByRequiredFlag" to "true",
+        "enumPropertyNaming" to "UPPERCASE",
+    )
+}
+
+// Workaround https://github.com/OpenAPITools/openapi-generator/issues/18904
+tasks.register<ReplaceInFilesTask>("dedupeSerializable") {
+    group = "Custom"
+    description = "Replaces occurrences of @Serializable interface in source files " +
+        "to work around the bug described at https://github.com/OpenAPITools/openapi-generator/issues/18904"
+
+    sourceDir.set(file("build/generate-resources/main/src"))
+    targetString.set("@Serializable\n\ninterface")
+    replacementString.set("interface")
+}
+
+abstract class ReplaceInFilesTask : DefaultTask() {
+    @get:InputDirectory
+    abstract val sourceDir: DirectoryProperty
+
+    @get:Input
+    abstract val targetString: Property<String>
+
+    @get:Input
+    abstract val replacementString: Property<String>
+
+    @TaskAction
+    fun replaceStrings() {
+        val target = targetString.get()
+        val replacement = replacementString.get()
+        val sourceDir = sourceDir.get().asFile
+
+        if (sourceDir.exists()) {
+            sourceDir.walkTopDown()
+                .filter { it.isFile }
+                .forEach { file ->
+                    val content = file.readText()
+                    val newContent = content.replace(target, replacement)
+                    if (content != newContent) {
+                        file.writeText(newContent)
+                        println("Replaced in: ${file.absolutePath}")
+                    }
+                }
+        } else {
+            println("Source directory does not exist: $sourceDir")
+        }
+    }
+}
+
+tasks.getByName("openApiGenerate").finalizedBy("dedupeSerializable")
