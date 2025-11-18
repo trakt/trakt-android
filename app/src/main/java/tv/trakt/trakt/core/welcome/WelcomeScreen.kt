36db8fb590ce6ba6135142afcf9dd7f691d72300
michaldrabik <michal.drabik0@gmail.com>
2025-11-18 21:38:17 +0100

    fix: welcome screen scrolling fix for smaller screens

--- app/src/main/java/tv/trakt/trakt/core/welcome/WelcomeScreen.kt
+++ app/src/main/java/tv/trakt/trakt/core/welcome/WelcomeScreen.kt
@@ -15,12 +15,15 @@ import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -63,10 +66,8 @@ import tv.trakt.trakt.ui.theme.TraktTheme
 @Composable
 internal fun WelcomeScreen(
     modifier: Modifier = Modifier,
-    onDismiss: () -> Unit = {},
+    onDismiss: () -> Unit,
 ) {
-    val uriHandler = LocalUriHandler.current
-
     val backgroundColor1 = TraktTheme.colors.backgroundPrimary
     val backgroundColor2 = Purple900.copy(alpha = 0.75F)
 
@@ -87,6 +88,8 @@ internal fun WelcomeScreen(
             .calculateBottomPadding(),
     )
 
+    val scrollState = rememberScrollState()
+
     Box(
         modifier = modifier
             .fillMaxSize()
@@ -101,10 +104,11 @@ internal fun WelcomeScreen(
         )
 
         Column(
-            verticalArrangement = Arrangement.SpaceBetween,
+            verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .fillMaxHeight()
+                .verticalScroll(scrollState)
                 .padding(contentPadding),
         ) {
             Row(
@@ -128,7 +132,10 @@ internal fun WelcomeScreen(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .padding(horizontal = 24.dp)
-                    .padding(bottom = 16.dp),
+                    .padding(
+                        top = 72.dp,
+                        bottom = 16.dp
+                    ),
             ) {
                 WelcomeItem(
                     title = "discover",
@@ -149,101 +156,18 @@ internal fun WelcomeScreen(
                 )
             }
 
-            val shape = RoundedCornerShape(24.dp)
-            Column(
-                verticalArrangement = spacedBy(16.dp),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 24.dp)
-                    .padding(bottom = 16.dp)
-                    .shadow(4.dp, shape)
-                    .clip(shape)
-                    .background(Purple900)
-                    .padding(
-                        start = 16.dp,
-                        end = 16.dp,
-                        top = 10.dp,
-                        bottom = 16.dp,
-                    ),
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = spacedBy(6.dp),
-                ) {
-                    Text(
-                        text = "15",
-                        style = TraktTheme.typography.heading1.copy(
-                            fontSize = 48.sp,
-                        ),
-                        color = Color.White,
-                        maxLines = 3,
-                        modifier = Modifier.graphicsLayer {
-                            translationY = 2.dp.toPx()
-                        },
-                    )
-
-                    Column {
-                        val font = TraktTheme.typography.paragraphSmaller.copy(fontSize = 12.sp)
-                        Text(
-                            text = "years of watching together",
-                            style = font,
-                            color = Color.White,
-                            maxLines = 1,
-                        )
-                        Text(
-                            text = "mil. shows & movies lovers",
-                            style = font,
-                            color = Color.White,
-                            maxLines = 1,
-                        )
-                        Text(
-                            text = "mil. titles tracked weekly",
-                            style = font,
-                            color = Color.White,
-                            maxLines = 1,
-                        )
-                    }
-
-                    Spacer(modifier = Modifier.weight(1F))
-
-                    Box(
-                        modifier = Modifier.onClick {
-                            uriHandler.openUri(WEB_ABOUT_US_URL)
-                        },
-                    ) {
-                        val users = remember {
-                            listOf(
-                                "https://walter-r2.trakt.tv/images/admins/000/000/001/avatars/thumb/2e6df69058.jpg",
-                                "https://walter-r2.trakt.tv/images/admins/000/000/002/avatars/thumb/0f9ac9e4e6.jpg",
-                                "https://walter-r2.trakt.tv/images/admins/000/410/291/avatars/thumb/348b7bf208.png",
-                            )
-                        }
-                        users.forEachIndexed { index, model ->
-                            AsyncImage(
-                                model = model,
-                                contentDescription = null,
-                                contentScale = ContentScale.Crop,
-                                error = painterResource(R.drawable.ic_person_placeholder),
-                                modifier = Modifier
-                                    .zIndex(10 - index.toFloat())
-                                    .padding(start = (index * 20).dp)
-                                    .size(42.dp)
-                                    .border(2.dp, Red500, CircleShape)
-                                    .clip(CircleShape),
-                            )
-                        }
-                    }
-                }
-
-                PrimaryButton(
-                    text = stringResource(R.string.button_text_join_trakt_free).uppercase(),
-                    containerColor = Purple500,
-                    contentColor = Color.White,
-                    onClick = onDismiss,
-                    modifier = Modifier.fillMaxWidth(),
-                )
-            }
+            Spacer(modifier = Modifier.height(256.dp))
         }
+
+        WelcomeFooter(
+            onDismiss = onDismiss,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(
+                    bottom = WindowInsets.navigationBars.asPaddingValues()
+                        .calculateBottomPadding()
+                )
+        )
     }
 }
 
@@ -318,9 +242,112 @@ private fun WelcomeItem(
     }
 }
 
+@Composable
+private fun WelcomeFooter(
+    modifier: Modifier = Modifier,
+    onDismiss: () -> Unit = {},
+) {
+    val uriHandler = LocalUriHandler.current
+
+    val shape = RoundedCornerShape(24.dp)
+    Column(
+        verticalArrangement = spacedBy(16.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp)
+            .padding(bottom = 16.dp)
+            .shadow(4.dp, shape)
+            .clip(shape)
+            .background(Purple900)
+            .padding(
+                start = 16.dp,
+                end = 16.dp,
+                top = 10.dp,
+                bottom = 16.dp,
+            ),
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = spacedBy(6.dp),
+        ) {
+            Text(
+                text = "15",
+                style = TraktTheme.typography.heading1.copy(
+                    fontSize = 48.sp,
+                ),
+                color = Color.White,
+                maxLines = 3,
+                modifier = Modifier.graphicsLayer {
+                    translationY = 2.dp.toPx()
+                },
+            )
+
+            Column {
+                val font = TraktTheme.typography.paragraphSmaller.copy(fontSize = 12.sp)
+                Text(
+                    text = "years of watching together",
+                    style = font,
+                    color = Color.White,
+                    maxLines = 1,
+                )
+                Text(
+                    text = "mil. shows & movies lovers",
+                    style = font,
+                    color = Color.White,
+                    maxLines = 1,
+                )
+                Text(
+                    text = "mil. titles tracked weekly",
+                    style = font,
+                    color = Color.White,
+                    maxLines = 1,
+                )
+            }
+
+            Spacer(modifier = Modifier.weight(1F))
+
+            Box(
+                modifier = Modifier.onClick {
+                    uriHandler.openUri(WEB_ABOUT_US_URL)
+                },
+            ) {
+                val users = remember {
+                    listOf(
+                        "https://walter-r2.trakt.tv/images/admins/000/000/001/avatars/thumb/2e6df69058.jpg",
+                        "https://walter-r2.trakt.tv/images/admins/000/000/002/avatars/thumb/0f9ac9e4e6.jpg",
+                        "https://walter-r2.trakt.tv/images/admins/000/410/291/avatars/thumb/348b7bf208.png",
+                    )
+                }
+                users.forEachIndexed { index, model ->
+                    AsyncImage(
+                        model = model,
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        error = painterResource(R.drawable.ic_person_placeholder),
+                        modifier = Modifier
+                            .zIndex(10 - index.toFloat())
+                            .padding(start = (index * 20).dp)
+                            .size(42.dp)
+                            .border(2.dp, Red500, CircleShape)
+                            .clip(CircleShape),
+                    )
+                }
+            }
+        }
+
+        PrimaryButton(
+            text = stringResource(R.string.button_text_join_trakt_free).uppercase(),
+            containerColor = Purple500,
+            contentColor = Color.White,
+            onClick = onDismiss,
+            modifier = Modifier.fillMaxWidth(),
+        )
+    }
+}
+
 @OptIn(ExperimentalCoilApi::class)
 @Preview(
-    device = "id:pixel_5",
+    device = "id:pixel_4",
     showBackground = true,
     backgroundColor = 0xFF131517,
 )
@@ -331,7 +358,9 @@ private fun Preview() {
             ColorImage(Color.Blue.toArgb())
         }
         CompositionLocalProvider(LocalAsyncImagePreviewHandler provides previewHandler) {
-            WelcomeScreen()
+            WelcomeScreen(
+                onDismiss = {},
+            )
         }
     }
 }
