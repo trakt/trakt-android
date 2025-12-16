130838450e089d950f17732418f4f520700afcaa
Michal <michal.drabik0@gmail.com>
2025-12-16 10:33:49 +0100

    chore: remove unused title fallback

--- common/src/main/java/tv/trakt/trakt/common/model/Episode.kt
+++ common/src/main/java/tv/trakt/trakt/common/model/Episode.kt
@@ -77,7 +77,7 @@ fun Episode.Companion.fromDto(dto: LastEpisodeDto): Episode {
         ids = Ids.fromDto(dto.ids),
         number = dto.number,
         season = dto.season,
-        title = dto.title ?: "TBA",
+        title = dto.title,
         numberAbs = dto.numberAbs,
         overview = dto.overview,
         rating = Rating(
