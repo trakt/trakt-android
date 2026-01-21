7e95dfb2de3fb4afe735de4c8dac4be38901f28e
Michal <michal.drabik0@gmail.com>
2026-01-21 18:02:48 +0100

    feat(calendar): state update

--- app/src/main/java/tv/trakt/trakt/core/calendar/CalendarState.kt
+++ app/src/main/java/tv/trakt/trakt/core/calendar/CalendarState.kt
@@ -1,11 +1,16 @@
 package tv.trakt.trakt.core.calendar
 
 import androidx.compose.runtime.Immutable
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.ImmutableMap
 import tv.trakt.trakt.common.helpers.LoadingState
 import tv.trakt.trakt.common.model.User
+import tv.trakt.trakt.core.home.sections.upcoming.model.HomeUpcomingItem
+import java.time.Instant
 
 @Immutable
 internal data class CalendarState(
+    val items: ImmutableMap<Instant, ImmutableList<HomeUpcomingItem>?>? = null,
     val user: User? = null,
     val loading: LoadingState = LoadingState.IDLE,
     val error: Exception? = null,
