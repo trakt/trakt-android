b7ee269a33b6fe869ad1a5cb426113354da7bde3
Michal <michal.drabik0@gmail.com>
2026-01-27 21:49:10 +0700

    fix: extra video date parsing issue fix

--- common/src/main/java/tv/trakt/trakt/common/model/ExtraVideo.kt
+++ common/src/main/java/tv/trakt/trakt/common/model/ExtraVideo.kt
@@ -1,9 +1,11 @@
 package tv.trakt.trakt.common.model
 
 import androidx.compose.runtime.Immutable
+import tv.trakt.trakt.common.helpers.extensions.nowUtc
 import tv.trakt.trakt.common.helpers.extensions.toZonedDateTime
 import tv.trakt.trakt.common.networking.ExtraVideoDto
 import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 @Immutable
 data class ExtraVideo(
@@ -25,7 +27,15 @@ data class ExtraVideo(
                 site = dto.site,
                 type = dto.type,
                 official = dto.official,
-                publishedAt = dto.publishedAt.toZonedDateTime(),
+                publishedAt = runCatching {
+                    dto.publishedAt.toZonedDateTime()
+                }.getOrElse {
+                    // Handle possible alternate date format: "2017-01-05 17:06:25 UTC"
+                    val altFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z")
+                    runCatching {
+                        ZonedDateTime.parse(dto.publishedAt, altFormat)
+                    }.getOrDefault(nowUtc())
+                },
             )
         }
     }
