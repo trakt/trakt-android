f6fcf99944968f8213b39503a810191a067e11a9
Michal <michal.drabik0@gmail.com>
2026-02-03 11:41:57 +0700

    feat(tv): shows/movies row limit update

--- tv/src/main/java/tv/trakt/trakt/app/core/movies/MoviesConfig.kt
+++ tv/src/main/java/tv/trakt/trakt/app/core/movies/MoviesConfig.kt
@@ -3,5 +3,5 @@ package tv.trakt.trakt.app.core.movies
 internal object MoviesConfig {
     const val MOVIES_PAGE_LIMIT = 50
     const val MOVIES_NEXT_PAGE_OFFSET = 10
-    const val MOVIES_SECTION_LIMIT = 15
+    const val MOVIES_SECTION_LIMIT = 10
 }
--- tv/src/main/java/tv/trakt/trakt/app/core/shows/ShowsConfig.kt
+++ tv/src/main/java/tv/trakt/trakt/app/core/shows/ShowsConfig.kt
@@ -3,5 +3,5 @@ package tv.trakt.trakt.app.core.shows
 internal object ShowsConfig {
     const val SHOWS_PAGE_LIMIT = 50
     const val SHOWS_NEXT_PAGE_OFFSET = 10
-    const val SHOWS_SECTION_LIMIT = 15
+    const val SHOWS_SECTION_LIMIT = 10
 }
