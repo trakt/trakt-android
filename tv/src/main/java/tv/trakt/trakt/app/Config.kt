38f4145a28759a67d6cbfe88bde4d2e50339dd64
Michal <michal.drabik0@gmail.com>
2026-02-04 15:46:13 +0700

    feat(tv): updated REFRESH_DATA_THRESHOLD_MINUTES from 5 to 3

--- tv/src/main/java/tv/trakt/trakt/app/Config.kt
+++ tv/src/main/java/tv/trakt/trakt/app/Config.kt
@@ -1,7 +1,7 @@
 package tv.trakt.trakt.app
 
 object Config {
-    const val REFRESH_DATA_THRESHOLD_MINUTES = 5L
+    const val REFRESH_DATA_THRESHOLD_MINUTES = 3L
     const val DEFAULT_PLEX_LOGO_URL =
         "trakt.tv/assets/channels/plex-5e7292839da5cdbdedb0f2806ec3fc8a6c5b436dd81ed7aeff825e68407c4ba2.png.webp"
 }
