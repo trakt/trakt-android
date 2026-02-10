e85e3df61a048f0b55053a7ae3b3a85af78faa3d
Michal <michal.drabik0@gmail.com>
2026-02-10 18:17:49 +0700

    feat(checkin): prepare band-aid episode checking endpint until OAPI update

--- app/src/main/java/tv/trakt/trakt/core/checkin/data/remote/CheckInApiClient.kt
+++ app/src/main/java/tv/trakt/trakt/core/checkin/data/remote/CheckInApiClient.kt
@@ -1,17 +1,18 @@
 package tv.trakt.trakt.core.checkin.data.remote
 
 import org.openapitools.client.apis.CheckinApi
+import org.openapitools.client.models.PostCheckinEpisodeRequest
+import org.openapitools.client.models.PostCheckinEpisodeRequestEp
 import org.openapitools.client.models.PostCheckinMovieRequest
 import org.openapitools.client.models.PostCheckinMovieRequestMovie
 import org.openapitools.client.models.PostCheckinMovieRequestMovieIds
 import tv.trakt.trakt.common.model.TraktId
-import tv.trakt.trakt.common.networking.CheckInMovieResponseDto
 
 class CheckInApiClient(
     private val api: CheckinApi,
 ) : CheckInRemoteDataSource {
-    override suspend fun postMovieCheckIn(movieId: TraktId): CheckInMovieResponseDto {
-        val result = api.postCheckinMovie(
+    override suspend fun postMovieCheckIn(movieId: TraktId) {
+        api.postCheckinMovie(
             postCheckinMovieRequest = PostCheckinMovieRequest(
                 movie = PostCheckinMovieRequestMovie(
                     PostCheckinMovieRequestMovieIds(
@@ -23,8 +24,29 @@ class CheckInApiClient(
                 ),
             ),
         )
+    }
 
-        return result.body()
+    override suspend fun postEpisodeCheckIn(
+        showId: TraktId,
+        season: Int,
+        episode: Int,
+    ) {
+        api.postCheckinEpisode(
+            postCheckinMovieRequest = PostCheckinEpisodeRequest(
+                show = PostCheckinMovieRequestMovie(
+                    ids = PostCheckinMovieRequestMovieIds(
+                        trakt = showId.value,
+                        slug = null,
+                        imdb = null,
+                        tmdb = -1,
+                    ),
+                ),
+                episode = PostCheckinEpisodeRequestEp(
+                    season = season,
+                    number = episode,
+                ),
+            ),
+        )
     }
 
     override suspend fun deleteAll() {
--- app/src/main/java/tv/trakt/trakt/core/checkin/data/remote/CheckInRemoteDataSource.kt
+++ app/src/main/java/tv/trakt/trakt/core/checkin/data/remote/CheckInRemoteDataSource.kt
@@ -1,10 +1,15 @@
 package tv.trakt.trakt.core.checkin.data.remote
 
 import tv.trakt.trakt.common.model.TraktId
-import tv.trakt.trakt.common.networking.CheckInMovieResponseDto
 
 interface CheckInRemoteDataSource {
-    suspend fun postMovieCheckIn(movieId: TraktId): CheckInMovieResponseDto
+    suspend fun postMovieCheckIn(movieId: TraktId)
+
+    suspend fun postEpisodeCheckIn(
+        showId: TraktId,
+        season: Int,
+        episode: Int,
+    )
 
     suspend fun deleteAll()
 }
