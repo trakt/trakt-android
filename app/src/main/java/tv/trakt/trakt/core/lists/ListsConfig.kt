6f28504585a7beb8870c21afeed174d2d6b10842
Michal <michal.drabik0@gmail.com>
2026-02-23 10:16:21 +0700

    feat(lists): updated section limit

--- app/src/main/java/tv/trakt/trakt/core/lists/ListsConfig.kt
+++ app/src/main/java/tv/trakt/trakt/core/lists/ListsConfig.kt
@@ -8,7 +8,7 @@ internal object ListsConfig {
     const val LIBRARY_PAGE_LIMIT = 200
 
     const val LISTS_MAX_PAGE_LIMIT = 1000
-    const val LISTS_SECTION_LIMIT = 40
+    const val LISTS_SECTION_LIMIT = 20
     const val LISTS_ALL_LIMIT = 100
 
     const val LISTS_FULL_VIEW_LIMIT = 3
