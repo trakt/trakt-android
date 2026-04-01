32dc37c9dd4b519d55e71c472e86acad5ce6c9c2
Michal <michal.drabik0@gmail.com>
2026-04-01 17:42:36 +0200

    chore: add free session manager for testing purposes.

---
+++ common/src/main/java/tv/trakt/trakt/common/auth/session/FreeSessionManager.kt
@@ -0,0 +1,82 @@
+package tv.trakt.trakt.common.auth.session
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.serialization.json.Json
+import timber.log.Timber
+import tv.trakt.trakt.common.auth.TokenProvider
+import tv.trakt.trakt.common.model.User
+
+// Token storage versioning.
+// Can be used to force clearing old data when the structure changes.
+private const val KEY_VERSION = 2
+
+private val KEY_USER_PROFILE = stringPreferencesKey("key_user_profile_$KEY_VERSION")
+
+// Non VIP session manager that doesn't store any VIP-related information and always returns false for VIP status.
+// Used only for some internal testing.
+internal class FreeSessionManager(
+    private val tokenProvider: TokenProvider,
+    private val dataStore: DataStore<Preferences>,
+) : SessionManager {
+    override suspend fun isAuthenticated(): Boolean {
+        return tokenProvider.getToken() != null
+    }
+
+    override suspend fun clear() {
+        tokenProvider.clear()
+        dataStore.edit { it.clear() }
+    }
+
+    override suspend fun saveProfile(user: User) {
+        val userData = Json.encodeToString(
+            user.copy(
+                isVip = false,
+                isVipEp = false,
+                isVipOg = false,
+            ),
+        )
+        dataStore.edit {
+            it[KEY_USER_PROFILE] = userData
+        }
+    }
+
+    override suspend fun getProfile(): User? {
+        val userData = dataStore.data.first()[KEY_USER_PROFILE] ?: return null
+        val decodedUserData = runCatching {
+            Json.decodeFromString<User?>(userData)?.copy(
+                isVip = false,
+                isVipEp = false,
+                isVipOg = false,
+            )
+        }.onFailure {
+            Timber.e(it, "Failed to decode user profile")
+        }
+        return decodedUserData.getOrElse {
+            clear()
+            null
+        }
+    }
+
+    override fun observeProfile(): Flow<User?> {
+        return dataStore.data.map { preferences ->
+            val data = preferences[KEY_USER_PROFILE]
+            if (data.isNullOrEmpty()) {
+                return@map null
+            }
+            return@map runCatching {
+                Json.decodeFromString<User>(data)
+            }.onFailure {
+                Timber.e(it, "Failed to decode user profile")
+            }.getOrElse {
+                clear()
+                null
+            }
+        }
+    }
+}
