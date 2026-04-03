54c1bebe9d559c2c59a1ce6aa7092b101c17237d
Michal <michal.drabik0@gmail.com>
2026-04-03 17:20:57 +0200

    feat(ui): update streaming logos tinting to ensure proper contrast

--- app/src/main/java/tv/trakt/trakt/core/summary/ui/views/DetailsStreamingItem.kt
+++ app/src/main/java/tv/trakt/trakt/core/summary/ui/views/DetailsStreamingItem.kt
@@ -39,6 +39,7 @@ import tv.trakt.trakt.common.model.streamings.StreamingType
 import tv.trakt.trakt.common.model.streamings.StreamingType.PURCHASE
 import tv.trakt.trakt.common.model.streamings.StreamingType.RENT
 import tv.trakt.trakt.ui.theme.TraktTheme
+import kotlin.math.pow
 
 @Composable
 internal fun DetailsStreamingItem(
@@ -46,15 +47,20 @@ internal fun DetailsStreamingItem(
     type: StreamingType,
     onClick: ((StreamingService) -> Unit)?,
 ) {
+    val context = LocalContext.current
+
     val itemHeight = 86.dp
     val contentHeight = 52.dp
+
+    val textColor = TraktTheme.colors.textPrimary
+    val containerColor = TraktTheme.colors.commentContainer
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = spacedBy(4.dp, CenterVertically),
         modifier = Modifier
             .height(itemHeight)
-            .background(TraktTheme.colors.commentContainer, RoundedCornerShape(16.dp))
+            .background(containerColor, RoundedCornerShape(16.dp))
             .padding(horizontal = 16.dp, vertical = 8.dp)
             .onClick(
                 onClick = { onClick?.invoke(service) },
@@ -63,7 +69,7 @@ internal fun DetailsStreamingItem(
         if (service.logo.isNullOrBlank()) {
             Text(
                 text = service.name,
-                color = TraktTheme.colors.textPrimary,
+                color = textColor,
                 style = TraktTheme.typography.buttonPrimary,
                 maxLines = 2,
                 overflow = TextOverflow.Ellipsis,
@@ -82,24 +88,34 @@ internal fun DetailsStreamingItem(
                 val withChannel = remember(service, type) {
                     !service.channel.isNullOrBlank() && type != StreamingType.FREE
                 }
-                val logoUrl = "https://${service.logo}"
-                AsyncImage(
-                    model = ImageRequest.Builder(LocalContext.current)
+
+                val logoUrl = remember(service.logo) { "https://${service.logo}" }
+                val logoRequest = remember(logoUrl) {
+                    ImageRequest.Builder(context)
                         .data(logoUrl)
                         .memoryCacheKey(logoUrl)
                         .placeholderMemoryCacheKey(logoUrl)
-                        .build(),
+                        .build()
+                }
+
+                val colorFilter = remember(service.color, containerColor) {
+                    ColorFilter.tint(
+                        when {
+                            service.color != null && getContrastRatio(service.color!!, containerColor) >= 3f -> {
+                                service.color!!
+                            }
+                            else -> {
+                                textColor
+                            }
+                        },
+                    )
+                }
+
+                AsyncImage(
+                    model = logoRequest,
                     contentDescription = null,
                     contentScale = ContentScale.FillHeight,
-                    colorFilter = remember(service.color) {
-                        ColorFilter.tint(
-                            when {
-                                service.color == Color.Black -> Color.White
-                                service.color != null -> service.color!!
-                                else -> Color.White
-                            },
-                        )
-                    },
+                    colorFilter = colorFilter,
                     modifier = Modifier.height(
                         when {
                             withChannel -> 34.dp
@@ -109,16 +125,22 @@ internal fun DetailsStreamingItem(
                 )
 
                 if (withChannel) {
-                    val logoUrl = "https://${service.channel}"
-                    AsyncImage(
-                        model = ImageRequest.Builder(LocalContext.current)
+                    val logoUrl = remember(service.channel) { "https://${service.channel}" }
+                    val logoRequest = remember(logoUrl) {
+                        ImageRequest.Builder(context)
                             .data(logoUrl)
                             .memoryCacheKey(logoUrl)
                             .placeholderMemoryCacheKey(logoUrl)
-                            .build(),
+                            .build()
+                    }
+
+                    AsyncImage(
+                        model = logoRequest,
                         contentDescription = null,
                         contentScale = ContentScale.FillHeight,
-                        colorFilter = ColorFilter.tint(Color.White),
+                        colorFilter = remember {
+                            ColorFilter.tint(textColor)
+                        },
                         modifier = Modifier
                             .height(20.dp)
                             .graphicsLayer {
@@ -162,6 +184,28 @@ internal fun DetailsStreamingItem(
             }
         }
     }
+}
+
+/**
+ * WCAG recommends a contrast ratio of at least 3:1 for UI components and graphical objects, and 4.5:1 for normal text.
+ * https://www.w3.org/TR/WCAG21/#contrast-minimum
+ */
+private fun getContrastRatio(
+    c1: Color,
+    c2: Color,
+): Float {
+    fun Float.linearize() =
+        if (this <= 0.04045f) {
+            this / 12.92f
+        } else {
+            ((this + 0.055f) / 1.055f).toDouble().pow(2.4).toFloat()
+        }
+
+    fun Color.luminance() = 0.2126f * red.linearize() + 0.7152f * green.linearize() + 0.0722f * blue.linearize()
+
+    val l1 = c1.luminance()
+    val l2 = c2.luminance()
+    return (maxOf(l1, l2) + 0.05f) / (minOf(l1, l2) + 0.05f)
 }
 
 @OptIn(ExperimentalCoilApi::class)
