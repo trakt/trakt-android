6e1e134fcf0feb86c632699b693ffed7bc900b36
Michal <michal.drabik0@gmail.com>
2026-05-11 10:51:20 +0200

    feat: update home calendar header

--- app/src/main/java/tv/trakt/trakt/core/home/sections/upcoming/HomeUpcomingView.kt
+++ app/src/main/java/tv/trakt/trakt/core/home/sections/upcoming/HomeUpcomingView.kt
@@ -11,12 +11,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -28,10 +26,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.google.firebase.Firebase
 import com.google.firebase.remoteconfig.remoteConfig
@@ -138,26 +134,9 @@ internal fun HomeUpcomingContent(
             ),
     ) {
         TraktSectionHeader(
-            title = stringResource(R.string.list_title_upcoming_schedule),
+            title = stringResource(R.string.page_title_calendar),
             chevron = state.user != null,
             collapsed = state.collapsed ?: false,
-            extraIcon = if (state.user != null) {
-                {
-                    Icon(
-                        painter = painterResource(R.drawable.ic_calendar),
-                        contentDescription = null,
-                        tint = TraktTheme.colors.textPrimary,
-                        modifier = Modifier
-                            .padding(start = 5.dp)
-                            .size(16.dp)
-                            .onClick {
-                                onCalendarClick()
-                            },
-                    )
-                }
-            } else {
-                null
-            },
             onCollapseClick = {
                 animateCollapse = true
                 val current = (state.collapsed ?: false)
