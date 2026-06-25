3df09de6b659e3fa2e457d5f193c7e066eed5bb7
Michal <michal.drabik0@gmail.com>
2026-06-25 08:27:03 +0200

    feat: new auth testing

--- app/src/main/java/tv/trakt/trakt/core/auth/ConfigAuth.kt
+++ app/src/main/java/tv/trakt/trakt/core/auth/ConfigAuth.kt
@@ -7,9 +7,8 @@ internal object ConfigAuth {
     const val OAUTH_REDIRECT_URI = "trakt://auth"
 
     val authCodeUrl: String
-        get() = "${Config.WEB_BASE_URL}oauth/authorize" +
+        get() = "${Config.WEB_BASE_URL_NEW}signin" +
             "?response_type=code" +
             "&client_id=${BuildConfig.TRAKT_API_KEY}" +
-            "&redirect_uri=$OAUTH_REDIRECT_URI" +
-            "&dark_knight=true"
+            "&redirect_uri=$OAUTH_REDIRECT_URI"
 }
