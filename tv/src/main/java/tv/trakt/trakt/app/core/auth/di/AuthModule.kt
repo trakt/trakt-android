79c15d7c85c3efd89173503f1c3061697a641f4e
Michal <michal.drabik0@gmail.com>
2026-07-01 09:44:22 +0200

    feat: update TV auth link

--- tv/src/main/java/tv/trakt/trakt/app/core/auth/di/AuthModule.kt
+++ tv/src/main/java/tv/trakt/trakt/app/core/auth/di/AuthModule.kt
@@ -10,14 +10,14 @@ import tv.trakt.trakt.app.core.auth.data.remote.AuthRemoteDataSource
 import tv.trakt.trakt.app.core.auth.usecases.GetDeviceCodeUseCase
 import tv.trakt.trakt.app.core.auth.usecases.GetDeviceTokenUseCase
 import tv.trakt.trakt.app.core.auth.usecases.LoadUserProfileUseCase
-import tv.trakt.trakt.common.Config.API_BASE_URL
+import tv.trakt.trakt.common.Config.WEB_AUTH_URL
 import tv.trakt.trakt.common.auth.TokenProvider
 
 internal val authDataModule = module {
     single<AuthRemoteDataSource> {
         AuthApiClient(
             api = OauthApi(
-                baseUrl = API_BASE_URL,
+                baseUrl = WEB_AUTH_URL,
                 httpClientEngine = get(),
                 httpClientConfig = get(named("clientConfig")),
             ),
