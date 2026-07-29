305c9177e9b758953b0360a9776622fe43a13766
Michal <michal.drabik0@gmail.com>
2026-07-29 13:11:01 +0200

    fix: search screen padding

--- tv/src/main/java/tv/trakt/trakt/app/core/search/SearchScreen.kt
+++ tv/src/main/java/tv/trakt/trakt/app/core/search/SearchScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.focusGroup
 import androidx.compose.foundation.layout.Arrangement.spacedBy
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -256,6 +257,12 @@ private fun SearchScreenContent(
                     )
                 }
             }
+
+            Spacer(
+                modifier = Modifier
+                    .focusProperties { canFocus = false }
+                    .padding(bottom = TraktTheme.spacing.mainContentVerticalSpace),
+            )
         }
     }
 }
